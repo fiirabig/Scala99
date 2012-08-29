1d0a518645557fcfeecfd02bc75b3104054b84a0
Guillaume Massé <guillaume.masse@polymtl.ca>
2012-08-29 17:24:33 -0400

    problem statements

---
+++ src/test/scala/ca/polymtl/log4420/scala99/WorkingWithListsTest.scala
@@ -0,0 +1,157 @@
+package ca.polymtl.log4420.scala99
+
+import ca.polymtl.log4420.scala99.WorkingWithLists._
+import org.scalatest._
+import matchers.ShouldMatchers
+
+class WorkingWithListsTest
+  extends FlatSpec
+  with ShouldMatchers
+{
+  "P01(*) last" should "Find the last element of a list." in
+  {
+
+    pending
+  }
+
+  "P02(*) penultimate" should "Find the last but one element of a list." in
+  {
+    pending
+  }
+
+  "P03(*) nth" should "Find the Kth element of a list." in
+  {
+    info("By convention, the first element in the list is element 0")
+    pending
+  }
+
+  "P04(*) length" should "Find the number of elements of a list." in
+  {
+    pending
+  }
+
+  "P05(*) reverse" should "Reverse a list." in
+  {
+    pending
+  }
+
+  "P06(*) isPalindrome" should "Find out whether a list is a palindrome." in
+  {
+    pending
+  }
+
+  "P07(**) flatten" should "Flatten a nested list structure." in
+  {
+    pending
+  }
+
+  "P08 (**) compress" should "Eliminate consecutive duplicates of list elements." in
+  {
+  pending
+  }
+
+  "P09 (**) pack" should "Pack consecutive duplicates of list elements into sublists." in
+  {
+    pending
+  }
+
+  "P10 (*) encode" should "Run-length encoding of a list." in
+  {
+    pending
+  }
+
+  "P11 (*) encodeModified" should "Modified run-length encoding." in
+  {
+    pending
+  }
+
+  "P12 (**) decode" should "Decode a run-length encoded list." in
+  {
+    pending
+  }
+
+  "P13 (**) encodeDirect" should "Run-length encoding of a list (direct solution)." in
+  {
+    pending
+  }
+
+  "P14 (*) duplicate" should "Duplicate the elements of a list." in
+  {
+    pending
+  }
+
+  "P15 (**) duplicateN" should "Duplicate the elements of a list a given number of times." in
+  {
+    pending
+  }
+
+  "P16 (**) drop" should "Drop every Nth element from a list." in
+  {
+    pending
+  }
+
+  "P17 (*) split" should "Split a list into two parts." in
+  {
+    pending
+  }
+
+  "P18 (**) slice" should "Extract a slice from a list." in
+  {
+    pending
+  }
+
+  "P19 (**) rotate" should "Rotate a list N places to the left." in
+  {
+    pending
+  }
+
+  "P20 (*) removeAt" should "Remove the Kth element from a list." in
+  {
+    pending
+  }
+
+  "P21 (*) insertAt" should "Insert an element at a given position into a list." in
+  {
+    pending
+  }
+
+  "P22 (*) range" should "Create a list containing all integers within a given range." in
+  {
+    pending
+  }
+
+  "P23 (**) randomSelect" should "Extract a given number of randomly selected elements from a list." in
+  {
+    pending
+  }
+
+  "P24 (*) lotto" should "Draw N different random numbers from the set 1..M." in
+  {
+    pending
+  }
+
+  "P25 (*) randomPermute" should "Generate a random permutation of the elements of a list." in
+  {
+    pending
+  }
+
+  "P26 (**) combinations" should "Generate the combinations of K distinct objects chosen from the N elements of a list." in
+  {
+    pending
+  }
+
+  "P27a (**) group3a" should "Group the elements of a set into disjoint subsets." in
+  {
+    pending
+  }
+
+  "P27b (**) group3b" should "b" in
+  {
+    pending
+  }
+
+  "P28 (**) lsort" should "Sorting a list of lists according to length of sublists." in
+    {
+      pending
+    }
+}
