14a17b8cf7419a763c4fbef037e2e4afbbe1f714
Guillaume Massé <guillaume.masse@polymtl.ca>
2012-08-29 18:09:13 -0400

    info from P14 to P23

--- src/test/scala/ca/polymtl/log4420/scala99/WorkingWithListsTest.scala
+++ src/test/scala/ca/polymtl/log4420/scala99/WorkingWithListsTest.scala
@@ -147,91 +147,244 @@ class WorkingWithListsTest
 
   "P12 (**) decode" should "Decode a run-length encoded list." in
   {
+    info("Given a run-length code list generated as specified in problem P10, construct its uncompressed version.")
+
+    /*
+      scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+      res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+     */
+
     pending
   }
 
   "P13 (**) encodeDirect" should "Run-length encoding of a list (direct solution)." in
   {
+    info("""
+     |Implement the so-called run-length encoding data compression method directly. I.e. don't use other methods
+     |you've written (like P09's pack); do all the work directly.
+     |""".stripMargin
+    )
+
+    /*
+    scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+    */
+
     pending
   }
 
   "P14 (*) duplicate" should "Duplicate the elements of a list." in
   {
+    /*
+    scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+    res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+     */
+
     pending
   }
 
   "P15 (**) duplicateN" should "Duplicate the elements of a list a given number of times." in
   {
+    /*
+    scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+    res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+     */
+
     pending
   }
 
   "P16 (**) drop" should "Drop every Nth element from a list." in
   {
+    /*
+    scala> drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    res0: List[Symbol] = List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
+     */
+
     pending
   }
 
   "P17 (*) split" should "Split a list into two parts." in
   {
+    info("The length of the first part is given. Use a Tuple for your result.")
+
+    /*
+    scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    */
+
     pending
   }
 
   "P18 (**) slice" should "Extract a slice from a list." in
   {
+    info("""
+      |Given two indices, I and K, the slice is the list containing the elements from and including the Ith
+      |element up to but not including the Kth element of the original list. Start counting the elements with 0.
+    """.stripMargin)
+
+    /*
+    scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    res0: List[Symbol] = List('d, 'e, 'f, 'g)
+    */
+
     pending
   }
 
   "P19 (**) rotate" should "Rotate a list N places to the left." in
   {
+    /*
+      scala> rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      res0: List[Symbol] = List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+
+      scala> rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      res1: List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+    */
     pending
   }
 
   "P20 (*) removeAt" should "Remove the Kth element from a list." in
   {
+    info("Return the list and the removed element in a Tuple. Elements are numbered from 0.")
+
+    /*
+    scala> removeAt(1, List('a, 'b, 'c, 'd))
+    res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+    */
+
     pending
   }
 
   "P21 (*) insertAt" should "Insert an element at a given position into a list." in
   {
+    /*
+    scala> insertAt('new, 1, List('a, 'b, 'c, 'd))
+    res0: List[Symbol] = List('a, 'new, 'b, 'c, 'd)
+    */
     pending
   }
 
   "P22 (*) range" should "Create a list containing all integers within a given range." in
   {
+    /*
+    scala> range(4, 9)
+    res0: List[Int] = List(4, 5, 6, 7, 8, 9)
+    */
     pending
   }
 
   "P23 (**) randomSelect" should "Extract a given number of randomly selected elements from a list." in
   {
+    /*
+    scala> randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+    res0: List[Symbol] = List('e, 'd, 'a)
+    */
+
+    info( "Hint: Use the solution to problem P20" )
+
     pending
   }
 
   "P24 (*) lotto" should "Draw N different random numbers from the set 1..M." in
   {
+    /*
+    scala> lotto(6, 49)
+    res0: List[Int] = List(23, 1, 17, 33, 21, 37)
+    */
     pending
   }
 
   "P25 (*) randomPermute" should "Generate a random permutation of the elements of a list." in
   {
+    info("Hint: Use the solution of problem P23.")
+
+    /*
+    scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+    res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+    */
+
     pending
   }
 
   "P26 (**) combinations" should "Generate the combinations of K distinct objects chosen from the N elements of a list." in
   {
+    info("""
+      |In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3)
+      |= 220 possibilities (C(N,K) denotes the well-known binomial coefficient). For pure mathematicians, this result
+      |may be great. But we want to really generate all the possibilities.
+    """.stripMargin)
+
+    /*
+    scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+    res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
+    */
+
     pending
   }
 
   "P27a (**) group3a" should "Group the elements of a set into disjoint subsets." in
   {
+    info(""""
+     |In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? Write a function
+     |that generates all the possibilities.
+    """.stripMargin)
+
+    /*
+    scala> group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+    res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David, Evi), List(Flip, Gary, Hugo, Ida)), ...
+    */
+
     pending
   }
 
-  "P27b (**) group3b" should "b" in
+  "P27b (**) group3b" should "Group the elements of a set into disjoint subsets." in
   {
+    info(""""
+       |Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return
+       |a list of groups.Note that we do not want permutations of the group members; i.e. ((Aldo, Beat), ...) is the
+       |same solution as ((Beat, Aldo), ...). However, we make a difference between ((Aldo, Beat), (Carla, David), ...)
+       |and ((Carla, David), (Aldo, Beat), ...).
+       |
+       |You may find more about this combinatorial problem in a good book on discrete mathematics
+       |under the term "multinomial coefficients".
+     """.stripMargin)
+
+    /*
+    scala> group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+    res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David), List(Evi, Flip, Gary, Hugo,
+    Ida)), ...
+    */
+
     pending
   }
 
-  "P28 (**) lsort" should "Sorting a list of lists according to length of sublists." in
-    {
-      pending
-    }
+  "P28a (**) lsort" should "Sorting a list of lists according to length of sublists." in
+  {
+    info(""""
+       |We suppose that a list contains elements that are lists themselves. The objective is to sort the elements of
+       |the list according to their length. E.g. that generates all the possibilities.
+     """.stripMargin)
+
+    /*
+    scala> lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o)))
+    res0: List[List[Symbol]] = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
+    */
+
+    pending
+  }
+
+  "P28b (**) lsort" should "Sorting a list of lists according to length of sublists." in
+  {
+    info(""""
+       |Again, we suppose that a list contains elements that are lists themselves. But this time the objective is to
+       |sort the elements according to their length frequency; i.e. in the default, sorting is done ascendingly,
+       |lists with rare lengths are placed, others with a more frequent length come later.
+     """.stripMargin)
+
+    /*
+    scala> lsortFreq(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o)))
+    res1: List[List[Symbol]] = List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
+    */
+
+    pending
+  }
 }
